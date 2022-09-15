119343d17dc7a57b19d28a807580d81e39a07b9a
ILikeMonday <worldrocker64@gmail.com>
2022-09-15 15:04:29 +0700

    not done

--- components/Comment.jsx
+++ components/Comment.jsx
@@ -1,13 +1,46 @@
+import { useRouter } from "next/router";
+import { comments } from "../libs/comments";
+import { useState } from "react";
 import React from "react";
 import Reply from "./Reply";
 
 export default function Comment(props) {
+  const [filterText, setFilterText] = useState("");
+  const filtered = comments.filter((x) =>
+    x.username.toLowerCase().includes(filterText.toLowerCase())
+  );
+
   return (
     <div>
       {/* Entire Comment div */}
-      <div></div>
+      {filtered.map((x) => (
+        <div className="d-flex gap-2 my-2" key={x.userImagePath}>
+          <img
+            src={x.userImagePath}
+            width="48"
+            height="48"
+            className="rounded-circle"
+            style={{ objectFit: "cover" }}
+          />
+          <div
+            className="rounded rounded-3 p-2"
+            style={{ backgroundColor: "#3A3B3C" }}
+          >
+            <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
+              {x.username}
+            </span>
+            <br />
+            <span style={{ color: "#E4E6EB" }}>{x.commentText}</span>
+            <div className="d-flex align-items-center gap-1">
+              <img src="/like.svg" width={20}></img>
+              <span style={{ color: "#B0B3B8" }}>{x.likeNum}</span>
+            </div>
+          </div>
+        </div>
+      ))}
 
       {/*render Reply here... */}
+      <Reply />
     </div>
   );
 }
--- components/Reply.jsx
+++ components/Reply.jsx
@@ -1,5 +1,45 @@
 import React from "react";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { comments } from "../libs/comments";
 
 export default function Reply(props) {
-  return <div></div>;
+  const [filterText, setFilterText] = useState("");
+  const filtered = comments.filter((x) =>
+    x.username.toLowerCase().includes(filterText.toLowerCase())
+  );
+
+  return (
+    <div>
+      {props.reply.map((x) => (
+        <div className="d-flex gap-2 my-2 ps-5" key={x.likeNum}>
+          <img
+            src={x.userImagePath}
+            width="48"
+            height="48"
+            className="rounded-circle"
+            style={{ objectFit: "cover" }}
+          />
+          <div
+            className="rounded rounded-3 p-2"
+            style={{ backgroundColor: "#3A3B3C" }}
+          >
+            <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
+              {x.username}
+            </span>
+            <br />
+            <span style={{ color: "#E4E6EB" }}>{x.replyText}</span>
+            {x.likeNum > 0 ? (
+              <div className="d-flex align-items-center gap-1">
+                <img src="/like.svg" width={20}></img>
+                <span style={{ color: "#B0B3B8" }}>{x.likeNum}</span>
+              </div>
+            ) : (
+              <div></div>
+            )}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
